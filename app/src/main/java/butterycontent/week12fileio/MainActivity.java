3f1e465b2205dde719b83378322c82a8261424f9
Elviis <jonathansakowski@Gmail.com>
2016-04-06 16:46:59 -0400

    Initial commit

---
+++ app/src/main/java/butterycontent/week12fileio/MainActivity.java
@@ -0,0 +1,111 @@
+package butterycontent.week12fileio;
+
+import android.content.Context;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.Console;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    //function to save a file for the save button click listener
+    public void saveToFile(View v){
+
+        EditText editText = (EditText) findViewById(R.id.etxtTextEntry);
+        String text = editText.getText().toString();
+        Log.d("Tag", text);
+
+        try{
+
+           FileOutputStream in = openFileOutput("testIOFile", Context.MODE_PRIVATE);
+            in.write(text.getBytes());
+            in.close();
+            Toast.makeText(this, "done", Toast.LENGTH_LONG).show();
+            editText.setText("");
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+    public void readFromFile(View v){
+
+        try{
+
+            FileInputStream in = openFileInput("testIOFile");
+            int c;
+            String temp = "";
+
+            while((c = in.read()) != -1){
+
+                temp = temp + Character.toString((char)c);
+
+            }
+
+            Toast.makeText(this, temp , Toast.LENGTH_LONG ).show();
+
+        }catch(Exception e){
+
+        }
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
